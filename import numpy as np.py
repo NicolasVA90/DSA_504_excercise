ea675a8e49e00113235284201b2be146612859ea
NicolasVA90 <vasquen@sunypoly.edu>
2026-09-16 11:51:20 -0400

    Pandas version

---
+++ import numpy as np.py
@@ -0,0 +1,11 @@
+#Python
+import numpy as np
+import pandas as pd
+
+print("NumPy version:", np.__version__)
+print("pandas version:", pd.__version__)
+
+
+import pandas as pd
+
+print("Ubicación de pandas:", pd.__file__)
